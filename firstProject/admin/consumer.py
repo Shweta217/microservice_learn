9fca91fd952295d80e19878656f071c2c159aa2e
Shweta217 <shweta.idk@gmail.com>
2022-02-12 20:24:26 +0000

    updated db with in-house db to mysql rds aws one

---
+++ firstProject/admin/consumer.py
@@ -0,0 +1,32 @@
+import pika, json, os, django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.settings")
+django.setup()
+
+from products.models import Product
+
+params = pika.URLParameters('amqps://tlontitk:MNGLzB3rtgSA-__gGASK-9oGNs2PSbKC@lionfish.rmq.cloudamqp.com/tlontitk')
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='admin')
+
+def callback(ch, method, properties, body):
+    print('Recieved in admin')
+    # print(body)
+    # data = json.loads(body)
+    # print(data)
+    id = json.loads(body)
+    print(id)
+    product = Product.objects.get(id=id)
+    product.likes = product.likes + 1
+    product.save()
+    print('Product likes increased! ')
+
+channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
+
+print('Started consuming')
+
+channel.start_consuming()
+
+channel.close()
